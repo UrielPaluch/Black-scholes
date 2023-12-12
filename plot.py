c58f0d5e75ed26a1bec109ab770c1ad588f59ad4
UrielPaluch <uripaluch.2@gmail.com>
2023-12-11 23:38:16 -0300

    Se modifica el grafico de salida, ahora esta todo en el mismo eje Y, se arregla la escala de la volatilidad del subyacente

--- plot.py
+++ plot.py
@@ -1,34 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
 
 # Lee el archivo CSV
 df = pd.read_csv('output.csv')
 
 # Crea un objeto de figura y ejes
-fig, ax1 = plt.subplots()
+fig, ax = plt.subplots()
 
 # Grafica la implied volatility en el eje Y principal
-ax1.plot(df['Created At'], df['Implied volatility'], label='Implied volatility', color='blue')
-ax1.scatter(df['Created At'], df['Implied volatility'], color='blue')  # Scatter para Implied volatility
-ax1.set_xlabel('Created At')
-ax1.set_ylabel('Implied volatility', color='blue')
-ax1.tick_params('y', colors='blue')
+ax.plot(df['Created At'], df['Implied volatility'], label='Implied volatility', color='blue')
+ax.scatter(df['Created At'], df['Implied volatility'], color='blue')  # Scatter para Implied volatility
 
-# Crea un segundo eje Y para la realized volatility
-ax2 = ax1.twinx()
-ax2.plot(df['Created At'], df['Under volatility'], label='Under volatility', color='green')
-ax2.scatter(df['Created At'], df['Under volatility'], color='green')  # Scatter para Under volatility
-ax2.set_ylabel('Under volatility', color='green')
-ax2.tick_params('y', colors='green')
+# Grafica la realized volatility en el mismo eje Y
+ax.plot(df['Created At'], df['Under volatility'], label='Under volatility', color='green')
+ax.scatter(df['Created At'], df['Under volatility'], color='green')  # Scatter para Under volatility
 
-# Rota los labels del eje x verticalmente en ambos ejes
-ax1.tick_params(axis='x', rotation=90)
-ax2.tick_params(axis='x', rotation=90)
+# Configura los labels del eje x verticalmente
+ax.tick_params(axis='x', rotation=90)
+
+# Configura los labels y el título
+ax.set_xlabel('Created At')
+ax.set_ylabel('Volatility')
+plt.title('Implied vs Realized Volatility')
 
 # Muestra la leyenda
-fig.tight_layout()
-plt.title('Implied vs Realized Volatility')
+ax.legend()
 
 # Muestra la imagen
 plt.show()
